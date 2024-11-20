773dfb6c1c4f5e890921a8c6490dd825359d2131
PranavvvSingh <pranavvalt@gmail.com>
2024-11-20 16:30:46 +0530

    add: fetch data in home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,60 +1,26 @@
 import { useEffect, useState } from "react"
 import apiClient from "../interceptor"
 
-// list all the users herer
+// list all the users here
 const Home = () => {
-   const [users, setUsers] = useState([
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-      { id: 301, name: "Alice Johnson" },
-      { id: 211, name: "Bob Smith" },
-      { id: 109, name: "Charlie Brown" },
-   ])
+   const [users, setUsers] = useState([])
 
    const [loading, setLoading] = useState(false)
 
-   // const fetchData = async () => {
-   //    setLoading(true)
-   //    try {
-   //       const resp = await apiClient("allUsers")
-   //       setUsers(resp.data)
-   //    } catch (error) {
-   //       console.log("Error fetching data: ", error)
-   //    }
-   //    setLoading(false)
-   // }
+   const fetchData = async () => {
+      setLoading(true)
+      try {
+         const resp = await apiClient("allUsers")
+         setUsers(resp.data)
+      } catch (error) {
+         console.log("Error fetching data: ", error)
+      }
+      setLoading(false)
+   }
 
-   // useEffect(() => {
-   //    fetchData()
-   // }, [])
+   useEffect(() => {
+      fetchData()
+   }, [])
 
    if (loading)
       return (
@@ -70,19 +36,21 @@ const Home = () => {
                <thead>
                   <tr className="text-blue-600 text-lg uppercase">
                      <th></th>
-                     <th className="">User Id</th>
+                     <th>User Id</th>
                      <th>Name</th>
+                     <th>Email</th>
                   </tr>
                </thead>
                <tbody className="text-base">
-                  {users.map((user, index) => (
+                  {users.map.length>0 && users.map((user, index) => (
                      <tr
                         key={index}
                         className="cursor-pointer hover:bg-blue-600/[0.1]"
                      >
                         <td>{index + 1}.</td>
                         <td>{user.id}</td>
-                        <td className="capitalize">{user.name}</td>
+                        <td className="capitalize">{user.username}</td>
+                        <td>{user.email}</td>
                      </tr>
                   ))}
                </tbody>
